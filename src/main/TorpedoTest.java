46b2c82ed18f949de483e2515f453bb0906246a3
hallgato hallgato <hallgato@oktatas.local>
2022-05-23 12:22:13 +0200

    hajo class

--- src/main/TorpedoTest.java
+++ src/main/TorpedoTest.java
@@ -4,5 +4,10 @@ package main;
 public class TorpedoTest {
     public static void main(String[] args) {
         System.out.println("Teszt");
+        
+        
+    }
+    public String tesztLoves (int poz){
+        return "";
     }
 }
---
+++ src/main/hajo.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * @author hallgato
+ */
+public class hajo {
+    int[] pozicio = new int[3];
+    
+public String talalat(int poz){
+    return "";
+}
+}
