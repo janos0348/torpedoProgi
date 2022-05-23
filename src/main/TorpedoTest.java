b2268a99a9cdf1205789674781dfeef73583049a
hallgato hallgato <hallgato@oktatas.local>
2022-05-23 12:36:45 +0200

    tesztloves

--- src/main/TorpedoTest.java
+++ src/main/TorpedoTest.java
@@ -2,12 +2,17 @@
 package main;
 
 public class TorpedoTest {
+    static hajo h1 = new hajo();
     public static void main(String[] args) {
+        new TorpedoTest().tesztLoves(4);
         System.out.println("Teszt");
         
+        assert 2==3 : "nem igaz";
         
     }
     public String tesztLoves (int poz){
+        String t = h1.talalat(poz);
+        assert t.equals("talált") : "nem jó a talalat";
         return "";
     }
 }
